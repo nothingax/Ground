2b906061767d65f3486fd941cc2c23521eaf8619
zhangjianwei <CSSVDD@gmail.com>
2019-08-01 20:24:18 +0800

    IDWorker update

--- ID-Generator/src/main/java/com/work/idworker/IDWorkerException.java
+++ ID-Generator/src/main/java/com/work/idworker/IDWorkerException.java
@@ -1,19 +1,6 @@
-/**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
+
 package com.work.idworker;
+
 /**
  * Program Name: Ground
  * <p>
@@ -26,21 +13,21 @@ package com.work.idworker;
  */
 public class IDWorkerException extends BaseException {
 
-  private static final long serialVersionUID = -329087741208425060L;
+    private static final long serialVersionUID = -329087741208425060L;
 
-  public IDWorkerException() {
-    super();
-  }
+    public IDWorkerException() {
+        super();
+    }
 
-  public IDWorkerException(String message) {
-    super(message);
-  }
+    public IDWorkerException(String message) {
+        super(message);
+    }
 
-  public IDWorkerException(String message, Throwable cause) {
-    super(message, cause);
-  }
+    public IDWorkerException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
-  public IDWorkerException(Throwable cause) {
-    super(cause);
-  }
+    public IDWorkerException(Throwable cause) {
+        super(cause);
+    }
 }
